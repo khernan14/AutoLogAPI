72f389f459c922655f7fc0b36e1f38985f1ad6a9
khernan14 <kevd_hernandezm@unicah.edu>
2025-05-31 20:44:05 -0600

    Cambiando datos errones

--- src/config/connectionToSql.js
+++ src/config/connectionToSql.js
@@ -1,22 +1,22 @@
-import mysql from "mysql2/promise";
+import mysql from "mysql2/promise"; // usa versión con promesas
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || "nozomi.proxy.rlwy.net",
+  host: process.env.DB_HOST || "mysql.railway.internal",
+  port: process.env.DB_PORT || 3306,
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "",
   database: process.env.DB_DATABASE || "railway",
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
-  ssl: {
-    rejectUnauthorized: true,
-  },
+  // ssl: { rejectUnauthorized: true }, ❌ NO uses esto para host interno
 });
 
 pool
   .getConnection()
-  .then(() => {
+  .then((conn) => {
     console.log("✅ Conexión exitosa a la base de datos MySQL");
+    conn.release();
   })
   .catch((err) => {
     console.error("❌ Error al conectar a la base de datos:", err.message);
